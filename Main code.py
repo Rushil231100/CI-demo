bd6c576e50bc924426f9927b895a21b23804da84
RUSHIL KAUSHAL SANGHAVI <50961163+Rushil231100@users.noreply.github.com>
2020-04-20 14:10:04 +0530

    Update Main code.py

--- Main code.py
+++ Main code.py
@@ -1,6 +1,6 @@
 # ci/ cd demo
 print("Hi Rushil")
-input_list = list(map(int , input().split()))
+input_list = list(map(int, input().split()))
 result = 0
 for each_input in input_list:
     result = result + each_input
