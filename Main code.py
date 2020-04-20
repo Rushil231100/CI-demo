d3ea2d43053c56b0110748e554be3cc6300b5d3b
RUSHIL KAUSHAL SANGHAVI <50961163+Rushil231100@users.noreply.github.com>
2020-04-20 13:47:48 +0530

    Update Main code.py

--- Main code.py
+++ Main code.py
@@ -1,10 +1,8 @@
 # ci/ cd demo
-print(" Hi Rushil")
+print("Hi Rushil")
 input_list = list(map(int , input().split()))
 result = 0
 for each_input in input_list:
     result = result + each_input
 
 print("Summation of list = {0}".format(result))
-
-
