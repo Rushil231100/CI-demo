0000e326ccacd656207b02dc1962807cdaff9600
RUSHIL KAUSHAL SANGHAVI <50961163+Rushil231100@users.noreply.github.com>
2020-04-20 13:37:32 +0530

    Update Main code.py

--- Main code.py
+++ Main code.py
@@ -1,9 +1,10 @@
 # ci/ cd demo
 print(" Hi Rushil")
-l=list(map(int,input().split()))
+input_list=list(map(int,input().split()))
 result =0
-for i in l:
-    result+=i
+for each_input in input_list:
+    result+=for each_input in input_list:
+
 print("Summation of list = {0}".format(result))
 
 
